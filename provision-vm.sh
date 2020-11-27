515861f9619e5b375502625282d400b9e704c49d
Eric Mountain <eric.mountain@datadoghq.com>
2020-11-27 16:01:27 +0100

    Ability to override dockerd binary + --no-cache
    --no-cache is useful when a custom dockerd builds broken layers...

--- provision-vm.sh
+++ provision-vm.sh
@@ -32,15 +32,39 @@ apt-get install -y docker-ce
 # ... or the Ubuntu-included version
 # apt-get install -y docker.io
 
+# If present, override dockerd with test binary
+if [ -e /home/vagrant/dockerd ] ; then
+  systemctl stop docker || true
+  cp /home/vagrant/dockerd /usr/local/bin/dockerd
+  chown root:root /usr/local/bin/dockerd
+  chmod 0755 /usr/local/bin/dockerd
+
+  [ ! -d /etc/systemd/system/docker.service.d ] && mkdir /etc/systemd/system/docker.service.d
+  cat - <<EOF > /etc/systemd/system/docker.service.d/dropin.conf
+[Service]
+ExecStart=
+ExecStart=/usr/local/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock
+EOF
+
+else
+  # Clear the drop-in in case we're re-provisioning
+  rm -f /etc/systemd/system/docker.service.d/dropin.conf 2> /dev/null
+fi
+
+systemctl daemon-reload
+systemctl start docker
+
+# Grant vagrant access to the docker socket
 usermod --append -G docker vagrant
 
 # Build image without user-namespaces
 rm -f /etc/docker/daemon.json 2> /dev/null
+systemctl reset-failed docker
 systemctl restart docker
 
 docker info
 
-sudo -i -u vagrant docker build -t capabilities-built-with-no-userns:1.0 ~vagrant
+sudo -i -u vagrant docker build --no-cache -t capabilities-built-with-no-userns:1.0 ~vagrant
 
 # Build image with user namespaces
 cat - <<EOF > /etc/docker/daemon.json
@@ -53,12 +77,13 @@ systemctl restart docker
 
 docker info
 
-sudo -i -u vagrant docker build -t capabilities-built-with-userns:1.0 ~vagrant
+sudo -i -u vagrant docker build --no-cache -t capabilities-built-with-userns:1.0 ~vagrant
 docker save capabilities-built-with-userns:1.0 > /tmp/capabilities-built-with-userns-1.0.tar
 
 # Now run both images in a no-user-ns setup
 
 rm -f /etc/docker/daemon.json 2> /dev/null
+systemctl reset-failed docker
 systemctl restart docker
 
 docker load < /tmp/capabilities-built-with-userns-1.0.tar
